3df8d5df67c703fb3afc958b042518dbf05dbfbc
wangfei <3188852847@qq.com>
2023-01-03 17:03:28 +0800

    first commit

---
+++ javaproject/src/method/method.java
@@ -0,0 +1,12 @@
+public class method {
+    public static void main(String[] args) {
+
+    }
+
+    //求两个数的和
+    public static int add(int a,int b){
+        int c = a+ b;
+        return c;
+    }
+}
+
