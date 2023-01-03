0c66b611d23978bb6d745b638955b721e8e40f7f
wangfei <3188852847@qq.com>
2023-01-03 13:31:48 +0800

    Initial commit

---
+++ javaproject/src/main/java/max.java
@@ -0,0 +1,11 @@
+public class max {
+    public static void main(String[] args) {
+        int arr[] = {10, 20, 30, 40};
+        int max = arr[1];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max)
+                max = arr[i];
+        }
+        System.out.println("数组的最大值是：" + max);
+    }
+}
